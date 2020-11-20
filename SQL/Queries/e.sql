6be1913259f23aa68d2acb5e70646eb943d52725
João Matos <joao.matos.jm@gmail.com>
2020-11-20 03:01:30 +0000

    Done f. fixed e.

--- SQL/Queries/e.sql
+++ SQL/Queries/e.sql
@@ -8,47 +8,63 @@ IF OBJECT_ID('nextCode') IS NOT NULL
 GO
 
 CREATE FUNCTION nextCode (@codeType VARCHAR(10))
-RETURNS VARCHAR(MAX) AS
-    BEGIN
-        DECLARE @current_code VARCHAR(MAX)
+    RETURNS VARCHAR(MAX) AS
+BEGIN
+    DECLARE @current_code VARCHAR(MAX)
 
-        IF(@codeType = 'invoice')
-            SELECT @current_code = code FROM Invoice
-        ELSE IF (@codeType = 'creditnote')
-            SELECT @current_code = code FROM CreditNote
-        ELSE
-            RETURN 'INVALID_PARAMETER'
-        SET @current_code = SUBSTRING(@current_code, 3, LEN(@current_code))
+    --Check if it is Invoice or CreditNote code
+    IF(@codeType = 'invoice')
+        SELECT @current_code = code FROM Invoice
+    ELSE IF (@codeType = 'creditnote')
+        SELECT @current_code = code FROM CreditNote
+    ELSE
+        RETURN 'INVALID_PARAMETER'
 
-        DECLARE @yearStr VARCHAR(MAX), @numStr VARCHAR(MAX)
-        DECLARE cursor_code CURSOR FOR
-            SELECT value FROM STRING_SPLIT(@current_code, '-')
-        OPEN cursor_code
-        FETCH NEXT FROM cursor_code INTO @yearStr
-        FETCH NEXT FROM cursor_code INTO @numStr
-        CLOSE cursor_code
-        DEALLOCATE cursor_code
+    --Check if it is the first code of the table (table was empty)
+    IF @current_code IS NULL
+        BEGIN
+            IF(@codeType = 'invoice')
+                RETURN CONCAT('FT', YEAR(GETDATE()), '-', 0)
+            ELSE IF (@codeType = 'creditnote')
+                RETURN CONCAT('NC', YEAR(GETDATE()), '-', 0)
+        END
 
-        DECLARE @year INT = CAST(@yearStr AS INT)
-        DECLARE @num INT = CAST(@numStr AS INT)
+    --Remove 'FT' or 'NC' from string
+    SET @current_code = SUBSTRING(@current_code, 3, LEN(@current_code))
 
-        IF (@year < YEAR(GETDATE()))
-            BEGIN
-                SET @year = YEAR(GETDATE())
-                SET @num = 0
-            END
-        ELSE
-            BEGIN
-                SET @num = @num + 1
-            END
+    --Extract year and number into string variables
+    DECLARE @yearStr VARCHAR(MAX), @numStr VARCHAR(MAX)
+    DECLARE cursor_code CURSOR FOR
+        SELECT value FROM STRING_SPLIT(@current_code, '-')
+    OPEN cursor_code
+    FETCH NEXT FROM cursor_code INTO @yearStr
+    FETCH NEXT FROM cursor_code INTO @numStr
+    CLOSE cursor_code
+    DEALLOCATE cursor_code
 
-        DECLARE @res VARCHAR(MAX)
-        IF(@codeType = 'invoice')
-            SET @res = CONCAT('FT', @year, '-', @num)
-        ELSE IF (@codeType = 'creditnote')
-            SET @res = CONCAT('NC', @year, '-', @num)
-        RETURN @res
-    END
+    --Cast them into int
+    DECLARE @year INT = CAST(@yearStr AS INT)
+    DECLARE @num INT = CAST(@numStr AS INT)
+
+    --See if it is the first code of the year, if not increment 1
+    IF (@year < YEAR(GETDATE()))
+        BEGIN
+            SET @year = YEAR(GETDATE())
+            SET @num = 0
+        END
+    ELSE
+        BEGIN
+            SET @num = @num + 1
+        END
+
+    --Build the string and return it
+    DECLARE @res VARCHAR(MAX)
+    IF(@codeType = 'invoice')
+        SET @res = CONCAT('FT', @year, '-', @num)
+    ELSE IF (@codeType = 'creditnote')
+        SET @res = CONCAT('NC', @year, '-', @num)
+    RETURN @res
+END
 GO
 
 --Test
@@ -56,6 +72,6 @@ DECLARE @res VARCHAR(MAX)
 EXEC @res = nextCode 'invoice'
 print @res
 
-INSERT INTO Invoice VALUES('FT2020-13456', NULL, 'updating', 10, 0, GETDATE(), NULL)
+INSERT INTO Invoice VALUES('FT2020-00006', NULL, 'updating', 10, 0, GETDATE(), NULL)
 INSERT INTO Invoice VALUES('FT2020-13458', NULL, 'updating', 10, 0, GETDATE(), NULL)
 INSERT INTO Invoice VALUES('FT2020-13460', NULL, 'updating', 10, 0, GETDATE(), NULL)
--- SQL/delete.sql
+++ SQL/delete.sql
@@ -13,10 +13,7 @@ IF OBJECT_ID('Item') IS NOT NULL
     END
 
 IF OBJECT_ID('CreditNote') IS NOT NULL
-    BEGIN
-        DBCC CHECKIDENT (CreditNote, RESEED, 0)
-        DELETE FROM CreditNote
-    END
+    DELETE FROM CreditNote
 
 IF OBJECT_ID('Product') IS NOT NULL
     DELETE FROM Product
@@ -28,10 +25,7 @@ IF OBJECT_ID('InvoiceHistory') IS NOT NULL
     END
 
 IF OBJECT_ID('Invoice') IS NOT NULL
-    BEGIN
-        DBCC CHECKIDENT (Invoice, RESEED, 0)
-        DELETE FROM Invoice
-    END
+    DELETE FROM Invoice
 
 IF OBJECT_ID('Contributor') IS NOT NULL
     DELETE FROM Contributor
